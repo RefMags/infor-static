cda9bf7857855e1910752ea570775109ffc964c0
RefMags <fyfymags@gmail.com>
2022-06-22 21:47:21 +0900

    nav

---
+++ src/components/MainContent.js
@@ -0,0 +1,9 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+
+export default function Maincontent() {
+  return (
+    <h1> MainContent goes here goes here</h1>
+  )
+}
---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+export default function Navbar() {
+  return (
+    <nav className="nav-main">
+      <img className="nav-logo" src="./images/logo.png" alt="" />
+      <h3 className="nav-title"> React Course -Project 1</h3>
+    </nav>
+  )
+}
