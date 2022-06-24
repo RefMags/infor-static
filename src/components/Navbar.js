b00896a60b7732e087350ecac56b18b489844a8e
RefMags <fyfymags@gmail.com>
2022-06-24 20:00:42 +0900

    nav-setting

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,9 +3,10 @@ import ReactDOM from "react-dom"
 
 export default function Navbar() {
   return (
-    <nav className="nav-main">
-      <img className="nav-logo" src="./images/logo.png" alt="" />
-      <h3 className="nav-title"> React Course -Project 1</h3>
+    <nav>
+      <img className="nav-logo" src="../images/logo.png" alt="" />
+      <h3> ReactFacts</h3>
+      <h4 className="nav-title"> React Course -Project 1</h4>
     </nav>
   )
 }
