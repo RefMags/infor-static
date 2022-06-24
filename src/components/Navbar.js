b58b31ebe0c60888e000b46581530c69eb3ac15d
RefMags <fyfymags@gmail.com>
2022-06-25 06:51:46 +0900

    nav-update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,9 +3,9 @@ import ReactDOM from "react-dom"
 
 export default function Navbar() {
   return (
-    <nav>
+    <nav className="nav-main">
       <img className="nav-logo" src="../images/logo.png" alt="" />
-      <h3> ReactFacts</h3>
+      <h3 className="nav-logo_title"> ReactFacts</h3>
       <h4 className="nav-title"> React Course -Project 1</h4>
     </nav>
   )
