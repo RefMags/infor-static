fb973b0ad84497d0a9b407dc4ef7cbaea75d3d75
RefMags <fyfymags@gmail.com>
2022-06-25 07:15:02 +0900

    static-progress-main added

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -5,8 +5,8 @@ import ReactDOM from "react-dom"
 export default function Maincontent() {
   return (
     <main>
-      <h1> Fun facts about React </h1>
-      <ul>
+      <h1 className="main-header"> Fun facts about React </h1>
+      <ul className="main-lists">
         <li> Was first released in 2013</li>
         <li> Was originally created by Jordan Walke</li>
         <li> Has well over 100K stars on GitHub</li>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,11 @@
 import React from "react"
 import ReactDOM from "react-dom"
+import reactLogo from "../images/reactjs-icon.png"
 
 export default function Navbar() {
   return (
     <nav className="nav-main">
-      <img className="nav-logo" src="../images/logo.png" alt="" />
+      <img className="nav-logo" src={reactLogo} alt="React Logo" />
       <h3 className="nav-logo_title"> ReactFacts</h3>
       <h4 className="nav-title"> React Course -Project 1</h4>
     </nav>
